e0c59704d2977b2d3b08c060dfb88ed7f849785c
d.balyaev <d.balyaev@npo-echelon.ru>
2023-03-03 09:05:35 +0300

    fix(config): add analise

--- config/build/buildWebpackPlugins.ts
+++ config/build/buildWebpackPlugins.ts
@@ -1,16 +1,40 @@
 import {WebpackBuildOptions} from "./types/build.types";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export function buildWebpackPlugins(options: WebpackBuildOptions): webpack.WebpackPluginInstance[] {
+    const { paths, isDev } = options;
 
-    const { paths } = options;
+    const progressPlugin = new webpack.ProgressPlugin();
 
     const htmlPlugin = new HtmlWebpackPlugin({
         template: paths.html
     });
 
-    const plugins = [htmlPlugin];
+    const miniCssPlugin = new MiniCssExtractPlugin({
+        filename: "css/[name].css",
+        chunkFilename: "css/[id].css",
+    });
+
+    const bundleAnalisePlugin = new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        reportTitle: () => {
+            const date = new Date();
+            return `DP-conspectus ${date.toLocaleDateString()}`
+        },
+    });
+
+    const plugins: webpack.WebpackPluginInstance[] = [htmlPlugin, progressPlugin];
+
+    if (!isDev) {
+        plugins.push(miniCssPlugin);
+    }
+
+    if (isDev) {
+        plugins.push(bundleAnalisePlugin);
+    }
 
     return plugins;
 }
